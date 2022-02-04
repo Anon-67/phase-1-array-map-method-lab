601714fd42298968672075e2295e0501ef2631e7
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-04 09:30:25 -0600

    commit tree b5847f46d3528d7a256c499805316c04b1487ee1

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(function(ele) {
+  ele = ele.split(' ').map(function(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  return ele.join(' ');
+});
+
+
+
+
+console.log(titleCased(tutorials));
